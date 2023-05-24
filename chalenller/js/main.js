8beecb78e15a44dd453a85d4462d2708963c2ad5
Jhcastro99 <103347624+Jhcastro99@users.noreply.github.com>
2023-05-24 11:49:36 -0500

    Update main.js
    cambios

--- chalenller/js/main.js
+++ chalenller/js/main.js
@@ -8,69 +8,16 @@ const mensajeTextSal = document.querySelector('.caja_mensajes');
 
 textEntrada.focus();
 
-//botones
+
 
 const botEncriptar = document.querySelector('.boton_encriptar');
 const botDesEncriptar = document.querySelector('.boton_deencriptar');
 const botCopy = document.querySelector('.copy');
 
 
-///////// botones funcionando
-
-/* botones, debemos pasar una funcion sin ser y ejecutarla nuestra dentro
-investigar por que, no se por que es necesario esto*/
-
-botEncriptar.addEventListener('click', function(){
-
-    if(textEntrada.value == ''){
-        alert('El campo esta vacio')
-    }else if(textEntrada.value != ''){
-        mostrarResult(encriptar(textEntrada.value));
-
-    }
-   
-    
-});
-botDesEncriptar.addEventListener('click', function(){
-
-    if(textEntrada.value == ''){
-        alert('El campo esta vacio')
-    }else if(textEntrada.value != ''){
-        mostrarResult(desEncriptar(textEntrada.value));
-    }
-
-});
 
 
-/// boton copy
 
-botCopy.addEventListener('click', copiarAlPortapapeles);
-
-
-/* tener en cuenta, se remplaza en orden por eso el orden es importante
-tuve un problema con el ai como estaba de primero al llegar a la i remplazaba
-la i del ai, EL ORDEN IMPORTA, SEGUN...*/
-function encriptar(text){
-
-    text = text.replace(/e/ig, "enter");
-    text = text.replace(/i/ig, "imes");
-    text = text.replace(/a/ig, "ai");
-    text = text.replace(/o/ig, "ober");
-    text = text.replace(/u/ig, "ufat");
-    return text;
-
-}
-
-function desEncriptar(text){
-
-    text = text.replace(/enter/g, "e");
-    text = text.replace(/imes/g, "i");
-    text = text.replace(/ai/g, "a");
-    text = text.replace(/ober/g, "o");
-    text = text.replace(/ufat/g, "u");
-    return text;
-
-}
 
 function copiarAlPortapapeles() {
 
@@ -109,3 +56,63 @@ function mostrarResult(text){
     
 
 }
+
+
+function encriptar(text){
+
+    text = text.replace(/e/ig, "enter");
+    text = text.replace(/i/ig, "imes");
+    text = text.replace(/a/ig, "ai");
+    text = text.replace(/o/ig, "ober");
+    text = text.replace(/u/ig, "ufat");
+    return text;
+
+}
+
+function desEncriptar(text){
+
+    text = text.replace(/enter/g, "e");
+    text = text.replace(/imes/g, "i");
+    text = text.replace(/ai/g, "a");
+    text = text.replace(/ober/g, "o");
+    text = text.replace(/ufat/g, "u");
+    return text;
+
+}
+
+function copiarAlPortapapeles() {
+
+    navigator.clipboard.writeText(textoSalida.value);
+    botCopy.value = 'COPIED';
+    console.log('copiado');
+    setTimeout(() =>{
+        botCopy.value = 'COPY';
+    }, 1000)
+
+
+}
+/// boton copy
+botCopy.addEventListener('click', copiarAlPortapapeles);
+
+
+
+botEncriptar.addEventListener('click', function(){
+
+    if(textEntrada.value == ''){
+        alert('El campo esta vacio')
+    }else if(textEntrada.value != ''){
+        mostrarResult(encriptar(textEntrada.value));
+
+    }
+   
+    
+});
+botDesEncriptar.addEventListener('click', function(){
+
+    if(textEntrada.value == ''){
+        alert('El campo esta vacio')
+    }else if(textEntrada.value != ''){
+        mostrarResult(desEncriptar(textEntrada.value));
+    }
+
+});
